3f3cda96e63a5f97cdad31d842901349ef170bf1
Andrey Tinyakov <tinyakov@greensight.ru>
2019-08-28 17:16:57 +0300

    Initial build subtree commit

---
+++ build/precache-manifest.357f163e89c81a9cd49399bd0c871526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3737716cb923e44142fd2bb03f7e2715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37e72a8bc02e22dc9a04",
+    "url": "/static/css/main.b0785adb.chunk.css"
+  },
+  {
+    "revision": "89c1dee689022a7e752e",
+    "url": "/static/js/2.144fec58.chunk.js"
+  },
+  {
+    "revision": "37e72a8bc02e22dc9a04",
+    "url": "/static/js/main.5b3b6a00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
