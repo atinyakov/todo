28c2e6ee288389ff79a90d4a2d4537903047164e
Andrey Tinyakov <tinyakov@greensight.ru>
2019-08-28 17:36:40 +0300

    Updates

---
+++ precache-manifest.2cdeeb8eacce8bbb177f7ef54ed07357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155e68b1eb652870c27d9b249d367ef8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "83ea98c0787fe53d5641",
+    "url": "/todo/static/css/main.b0785adb.chunk.css"
+  },
+  {
+    "revision": "89c1dee689022a7e752e",
+    "url": "/todo/static/js/2.144fec58.chunk.js"
+  },
+  {
+    "revision": "83ea98c0787fe53d5641",
+    "url": "/todo/static/js/main.70159d87.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
