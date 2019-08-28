0c6c27fde49aedcf14e9f0f75b315724e50d688e
Andrey Tinyakov <tinyakov@greensight.ru>
2019-08-28 17:23:35 +0300

    removed extra build folder

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -20,7 +20,7 @@ export default class SearchPanel extends Component {
       <input type="text"
              className="form-control search-input"
              placeholder="type to search"
-            //  value={this.state.term}
+             value={this.state.term}
              onChange={ this.onTermChange } />
     );
   };
